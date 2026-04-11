22f078718c6f547988de1fe8da410b4e738f9ba6
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-11 19:29:54 -0300

    refatorado o método cadastrar com o validarLivro

--- LivroService.java
+++ LivroService.java
@@ -6,26 +6,7 @@ public class LivroService {
     private List<Livro> acervo = new ArrayList<>();
 
     public void cadastrar(Livro novoLivro) throws Exception {
-        if (novoLivro == null)
-            throw new Exception("objeto nulo.");
-
-        if (novoLivro.getTitulo() == null || novoLivro.getTitulo().isEmpty())
-            throw new Exception("Título inválido.");
-        novoLivro.setTitulo(novoLivro.getTitulo().trim().toUpperCase());
-
-        if (novoLivro.getAutor() == null || novoLivro.getAutor().isEmpty())
-            throw new Exception("Autor inválido.");
-        novoLivro.setAutor(novoLivro.getAutor().trim().toUpperCase());
-
-        if (novoLivro.getAnoPublicacao() < 1900 || novoLivro.getAnoPublicacao() > LocalDate.now().getYear())
-            throw new Exception("Ano de publicação inválido");
-
-        for (Livro livro : acervo) {
-            if (livro.getTitulo().equalsIgnoreCase(novoLivro.getTitulo())
-                    && livro.getAutor().equalsIgnoreCase(novoLivro.getAutor())
-                    && livro.getAnoPublicacao() == novoLivro.getAnoPublicacao())
-                throw new Exception("Já existe livro cadastrado com esse Título, Autor e ano de publicação.");
-        }
+        validarLivro(novoLivro, -1);
 
         // nesta parte estaria chamando a camada Repository
 
