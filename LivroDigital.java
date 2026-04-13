caff63fceb3e6d1309f259e9b233125e7e6b19a4
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-04-13 19:37:51 -0300

    Criado duas novas classes com getters e setters: LivroDigital e LivroFisico

---
+++ LivroDigital.java
@@ -0,0 +1,17 @@
+public class LivroDigital extends Livro {
+    private double tamanhoArquivo;
+    private String formatoArquivo;
+
+    public double getTamanhoArquivo() {
+        return tamanhoArquivo;
+    }
+    public void setTamanhoArquivo(double tamanhoArquivo) {
+        this.tamanhoArquivo = tamanhoArquivo;
+    }
+    public String getFormatoArquivo() {
+        return formatoArquivo;
+    }
+    public void setFormatoArquivo(String formatoArquivo) {
+        this.formatoArquivo = formatoArquivo;
+    }
+}
---
+++ LivroFisico.java
@@ -0,0 +1,17 @@
+public class LivroFisico extends Livro {
+    private int numeroExemplares;
+    private String dimensoes;
+
+    public int getNumeroExemplares() {
+        return numeroExemplares;
+    }
+    public void setNumeroExemplares(int numeroExemplares) {
+        this.numeroExemplares = numeroExemplares;
+    }
+    public String getDimensoes() {
+        return dimensoes;
+    }
+    public void setDimensoes(String dimensoes) {
+        this.dimensoes = dimensoes;
+    }
+}
